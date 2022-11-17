0c01a22624b69f96b1c14ecfbc9774f215a850a1
w32siooo <creeco@gmail.com>
2022-11-17 09:25:28 +0100

    batch

--- backend/src/main/java/cygni/denmark/moviesearchservice/tasks/IngestDirigentTask.java
+++ backend/src/main/java/cygni/denmark/moviesearchservice/tasks/IngestDirigentTask.java
@@ -30,30 +30,37 @@ public class IngestDirigentTask {
 
   private final ActorDocIngestTask actorDocIngestTask;
 
+  private boolean ingestToElastic = false;
+
   @Scheduled(fixedDelay = Long.MAX_VALUE, initialDelay = 1000)
   public void scheduleFixedRateWithInitialDelayTask() {
     log.info("scheduled task started");
     long startTime = Instant.now().toEpochMilli();
     cleanElasticService
         .cleanRepos()
-      //  .then(actorDbIngestTask.run())
+       // .then(actorDbIngestTask.run())
         .doOnSuccess(inserted -> log.info("{} Actor elements indexed in postgres ", inserted))
-     //   .then(movieDbIngestTask.run())
+       // .then(movieDbIngestTask.run())
         .doOnSuccess(inserted -> log.info("{} Movie elements indexed in postgres ", inserted))
         .block();
+    actorDocIngestTask.streamToElasticFromJpaAndBlock(1000000, "2022-11-16 15:22:34.093");
 
-    var res = actorDocIngestTask.streamToElasticFromJpaAndBlock(100000, "2022-11-16 15:22:34.093");
-    for (int i = 0; i < 8; i++) {
-      var newRes = actorDocIngestTask.streamToElasticFromJpaAndBlock(100000, res.toString());
-      if (newRes == null) {
-        actorDocIngestTask.streamToElasticFromJpaAndBlock(100000, res.toString());
-      } else {
-        res = newRes;
+    if (ingestToElastic) {
+      var res =
+          actorDocIngestTask.streamToElasticFromJpaAndBlock(100000, "2022-11-16 15:22:34.093");
+      for (int i = 0; i < 8; i++) {
+        log.info("progress: "+"=".repeat(i));
+        var newRes = actorDocIngestTask.streamToElasticFromJpaAndBlock(100000, res.toString());
+        if (newRes == null) {
+          actorDocIngestTask.streamToElasticFromJpaAndBlock(100000, res.toString());
+        } else {
+          res = newRes;
+        }
       }
-    }
-    log.info("actor elements also indexed in elastic" + res);
+      log.info("actor elements also indexed in elastic" + res);
 
-    movieDocIngestTask.streamToElasticFromJpaAndBlock();
+     // movieDocIngestTask.streamToElasticFromJpaAndBlock();
+    }
     long endTime = Instant.now().toEpochMilli() - startTime;
     log.info(
         "scheduled task ended, it took {} seconds and {} miliseconds",
