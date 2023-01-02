204d2302c854cd50306eb57974d2659a61233ca0
w32siooo <creeco@gmail.com>
2023-01-02 14:48:11 +0100

    statecontroller

--- backend/src/main/java/cygni/denmark/moviesearchservice/controllers/StateController.java
+++ backend/src/main/java/cygni/denmark/moviesearchservice/controllers/StateController.java
@@ -1,5 +1,7 @@
 package cygni.denmark.moviesearchservice.controllers;
 
+import cygni.denmark.moviesearchservice.persistence.repositories.ActorRepository;
+import cygni.denmark.moviesearchservice.persistence.repositories.MovieRepository;
 import cygni.denmark.moviesearchservice.persistence.repositories.StateValue;
 import cygni.denmark.moviesearchservice.services.StateService;
 import cygni.denmark.moviesearchservice.tasks.IngestDirigentTask;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @RestController
 @RequestMapping("/api/state")
@@ -22,6 +25,10 @@ class StateController {
 
     private final IngestDirigentTask ingestDirigentTask;
 
+    private final MovieRepository movieRepository;
+
+    private final ActorRepository actorRepository;
+
      @GetMapping(value = "", produces = "application/json")
     public Mono<StateValue> getState() {
         return stateService.getState();
@@ -31,6 +38,18 @@ class StateController {
     public Mono<String> updateState() {
          ingestDirigentTask.scheduleFixedRateWithInitialDelayTask();
          return Mono.just("Scheduled");
+    }
+
+    @GetMapping(value = "actorCount", produces = "application/json")
+    public Mono<Long> getActorCount() {
+        return Mono.fromCallable(actorRepository::count)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @GetMapping(value = "movieCount", produces = "application/json")
+    public Mono<Long> getMovieCount() {
+        return Mono.fromCallable(movieRepository::count)
+                .subscribeOn(Schedulers.boundedElastic());
     }
 
  }
